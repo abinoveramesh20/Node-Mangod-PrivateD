90bca06164046745073c49db0bad66e92b99ba65
Abinove Ramesh <145929189+abinoveramesh20@users.noreply.github.com>
2024-03-25 16:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 // Welcome message for root route
 app.get('/', (req, res) => {
-  res.send('Say Hi to the Todo App!');
+  res.send('You can do it!');
 });
 
 // Routes
