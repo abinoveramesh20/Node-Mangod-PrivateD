a98091e69c6bfddd6445add794991dbd74d1e8fd
Abinove Ramesh <145929189+abinoveramesh20@users.noreply.github.com>
2024-03-25 17:48:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 // Welcome message for root route
 app.get('/', (req, res) => {
-  res.send('WE can do it! Yess WE can');
+  res.send('WE can do it!  WE can');
 });
 
 // Routes
